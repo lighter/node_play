e72e270b74b991a2bf7f0b807ed5ebac141d4d6b
lighter <apple.xva@gmail.com>
2014-11-07 11:52:13 +0800

    add login page

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components/bootstrap/dist/')));
+app.use(express.static(path.join(__dirname, 'bower_components/jquery/dist/')));
 
 app.use('/', routes);
 app.use('/users', users);
